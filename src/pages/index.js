049c4acafe0050b1372a98a0060631cf869c125d
Mateo Clarke <code@mateoclarke.com>
2017-09-27 14:28:34 -0500

    Initial Gatsby Config (#46)
    * Adds two gatsby plugins. One for filesystem reading, another to transform markdown.
    * Pulls data into pages via 1) gatsby-config.js siteMetadata 2) src/pages/**/*.md
    * Index Page pulls in project listing from src/pages/projects directory.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default ({ data }) => {
+  const postLength = data.allMarkdownRemark.edges.length;
 
-export default IndexPage
+  return (
+    <div>
+      <h1>Projects</h1>
+      <h4>{postLength} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => {
+        return (
+          <div className="" key={node.id}>
+            <h3>{node.frontmatter.title}</h3>
+            <p>{node.excerpt}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export const query = graphql`
+  query AllMarkdownsQuery {
+    allMarkdownRemark(filter: {frontmatter: { layout: { eq: "project" } }}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            layout
+            path
+            date
+            parent
+          }
+          html
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/project/project-1.md
@@ -0,0 +1,7 @@
+---
+layout: project
+path: Project A
+date: 2017-09-22T01:22:58.061Z
+title: Project A
+---
+project b is another project
