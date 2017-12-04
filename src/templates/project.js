4c39d488a3735771011db8bd8d206d41b2e98862
toribr <tori.breitling@austintexas.gov>
2017-12-04 10:33:11 -0600

    Markup & styling of project detail

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,7 @@
 import React from "react"
 import {FormattedNumber} from "react-intl"
+import "./Projects.scss"
+import clock from "/usr/local/bin/grackl/src/zilker/img/clock.png"
 
 export default ({ data }) => {
   const {
@@ -9,51 +11,72 @@ export default ({ data }) => {
   } = data.airtable;
 
   return (
-    <div className="page-section">
-      <h1>
-        {projectName}
+    <section>
+      <div className="usa-grid-full">
+    <div className="usa-width-three-quarters">
+      <h1 className="coa-project_name">
+        {projectName} 
       </h1>
+<h2 className="coa-project_summary">
+{projectSummary}</h2>
+<div className="coa-project_summary">
 
+{ link && <a href={link}>Learn more about this project.</a>}</div>
+</div>
+</div>
 
-      <div>
-        <h3>Project stage: {projectStage}</h3>
-        {projectSummary}
-      </div>
+<div className="usa-grid-full">
 
-      { link && <div><a href={link}>Learn more about this project.</a></div> }
+        <h3 className="coa-project_phase"> <img src="/zilker/img/Clock.png" alt="clock" /> {projectStage}</h3>
+      
+      <hr></hr>
+      <div className="usa-grid-full">
+      <div className="usa-width-one-fourth">
 
-      <div>
-        <h2>Funding</h2>
-        <p>Total budget of &nbsp;
+        <h2 className="coa-city_sub-category">Funding</h2></div>
+       
+       <div className="usa-width-three-fourths"> 
+       <p>Total budget of &nbsp;
           <FormattedNumber value={totalProjectBudget} style="currency" currency="USD" minimumFractionDigits={0} />
-        </p>
-        <p>{fundingSource}</p>
-      </div>
-
-      <div>
-        <h2>Who's Involved</h2>
-        <p>Project champions: {leadDepartment}</p> {/* TODO: Currently getting a reference ID. Need Dept name */}
+        <br />{fundingSource}</p>
+        
+        </div>
+        </div>
+    
+        <hr></hr>
+     <div className="usa-grid-full"> 
+     <div className="usa-width-one-fourth">
+      <h2 className="coa-city_sub-category">Who's Involved</h2></div>
+        
+        <div className="usa-width-three-fourths">
+        <p>Project champions: {leadDepartment}
+        {/* TODO: Currently getting a reference ID. Need Dept name */}</p>
         { partnerOrganizations &&
           <p>Partner organizations: &nbsp;
             <a href={linksToAnyPartnerOrganizationWebsites}>
               {partnerOrganizations}
-            </a>
-          </p>
+            </a></p>   
         }
-      </div>
-
-      <div>
-        <h2>Project Goals</h2>
+      </div></div>
+      <hr></hr>
+      <div className="usa-grid-full">
+      <div className="usa-width-one-fourth">
+        <h2 className="coa-city_sub-category">Project Goals</h2></div>
+        <div className="usa-width-three-fourths"> 
         <p>{cityStrategicOutcomes}</p>
-      </div>
-
-      <div>
-        <h2>Get in Touch</h2>
-        <p>{contactName}</p>
-        <p>{contactEmail}</p>
-      </div>
-
-    </div>
+      </div></div>
+      <hr></hr>
+      <div className="usa-grid-full">
+      <div className="usa-width-one-fourth">
+        <h2 className="coa-city_sub-category">Get in Touch</h2>
+        </div>
+        <div className="usa-width-three-fourths"> 
+        <p>{contactName}
+        <br />{contactEmail}</p>
+      </div></div>
+</div>
+  </section>
+    
   )
 }
 
