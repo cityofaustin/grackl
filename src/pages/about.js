42f9ff330859c84ffd01a2ec70aea52a564795a7
toribr <tori.breitling@austintexas.gov>
2017-12-27 07:10:11 -0600

    About page
    New layout and edited copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,55 +1,141 @@
 import React from "react"
 import PrimaryButton from "../lib/components/PrimaryButton";
 import Link from 'gatsby-link'
+import ClockSvg from "../zilker/ClockSvg.js"
+import LearningSvg from "../zilker/LearningSvg.js"
+import MobilitySvg from "../zilker/MobilitySvg.js"
+import GovSvg from "../zilker/GovSvg.js"
+import HealthSvg from "../zilker/HealthSvg.js"
+import SafetySvg from "../zilker/SafetySvg.js"
+import EconomicSvg from "../zilker/EconomicSvg.js"
 import "../zilker/scss/About.scss"
 
 
 
 const AboutPage = ({ data }) => (
-  <div className="usa-grid-full">
-    <div><h1 className="coa-about_head">
-      About {data.site.siteMetadata.title}
-    </h1>
-    <div className="coa-about_body">
-    ATX Project Tracker aims to make it easier for people in Austin to have a better understanding of what projects the City is working on, the state of progress of those projects, how the projects relate to critical goals, and how they are funded.
+ 
+  <section className="usa-section usa-grid">
+  <div className="row">
+  <div className="col-xs-12 col-sm-8">
+      <h2 className="coa-about__head">About {data.site.siteMetadata.title}
+      </h2>
+
+      <p className="coa-about__body">
+      ATX Project Tracker aims to make it easier for people in Austin to have a better understanding of:
+      
+        <ul>• What projects the City is working on,</ul>
+        <ul>• The state of progress of those projects,</ul>
+        <ul>• How the projects relate to critical goals,</ul>
+        <ul>• And how they are funded.</ul>
+        
+      The City of Austin consists of 40+ departments and over 13,000 employees serving almost 1 million people. There are tons of City-sponsored projects going on at any given moment in time – street resurfacing, park improvements, service delivery, and research, to name a few. 
+      </p>
+      <p className="coa-about__body">It can be challenging for people in Austin to know what projects are going on relevant to them, what their status is, and how they can learn more (or even participate). City staff often face the same challenge. Because many of Austin’s challenges cross departmental lines–such as housing affordability, which involves at least half a dozen departments–many employees need easier access to information on other City projects that relate to their work.
+      </p>
+      <p className="coa-about__body">When the City of Austin joined the Open Government Partnership’s Subnational Pilot Program in December 2016, we made a commitment to build and test a project tracking tool as part of our participation. In short, we committed to up the City of Austin’s game in transparency, civic participation, accountability, and technology innovation. <a href="https://www.opengovpartnership.org/about/about-ogp" target="_blank" class="link--external">Learn more about the Open Government Partnership</a>
+      </p>
+    <p className="coa-about__title">
+      Other City of Austin Project Tracking Tools
+   </p>
+      <p className="coa-about__body">
+        If you are looking for details about projects and programs funded by the 2016 Mobility Bond Program, you can view them on the <a href="http://capitalprojects.austintexas.gov/projects" target="_blank" class="link--external">City of Austin Project Explorer.</a> If you are interested in diving into the City's budget and how funds are allocated, get a guided view through <a href="http://budget.austintexas.gov/#!/year/default"> Open Budget ATX.</a>
+      </p>
+    <p className="coa-about__title">
+      Follow our progress and contribute 
+    </p>
+      <p className="coa-about__body">
+        Since this is open government, we are designing and building a project tracking tool using open source code that users can follow, copy and contribute to. We keep our code on <a href="https://github.com/cityofaustin/grackl">Github</a> and welcome collaboration!
+      </p>
+
+    <p className="coa-about__title">Get In Touch
+    </p>
+      <p className="coa-about__body">
+      Do you have feedback for us? Or are you a City staff member wanting to publish your project on ATX Project Tracker?
+      We would love to hear from you:<br/>
+      Contact us at the: <a href="mailto:innovation@austintexas.gov">Innovation Office</a>
+      </p>
+   </div>
+  
+    <div className="col-xs-12 col-sm-4">
+   <div className="coa-About__sidebar__head"> <ClockSvg />
+      Phases
+    </div>
+    <div className="coa-About__sidebar__title"></div>
+   
     
-    The City of Austin consists of 40+ departments and over 13,000 employees serving almost 1 million people. There are tons of City-sponsored projects going on at any given moment in time – street resurfacing, park improvements, service delivery, and research, to name a few. It can be challenging for people in Austin to know what projects are going on relevant to them, what their status is, and how they can learn more (or even participate). City staff often face the same challenge. Because many of Austin’s challenges cross departmental lines–such as housing affordability, which involves at least half a dozen departments–many employees need easier access to information on other City projects that relate to their work.
+      <dl className="coa-about__phase">
+        <dt className="coa-About__sidebar">PLANNING</dt>
+          <dd className="coa-About__sidebar__plain-text">Identifying, qualifying, and scoping a project. Includes leads or opportunities that have just been identified, but haven't yet decided to pursue.
+          </dd>
+        <dt className="coa-About__sidebar">LEARNING</dt>
+          <dd className="coa-About__sidebar__plain-text">Includes research, discovery, needs assessment, and requirements gathering
+          </dd>
+        <dt className="coa-About__sidebar">IMPLEMENTING</dt>
+          <dd className="coa-About__sidebar__plain-text">Execution and active project work, including design, development, testing, solution implementation.
+          </dd>
+        <dt className="coa-About__sidebar">COMPLETE/LIVE</dt>
+          <dd className="coa-About__sidebar__plain-text">Finished implementation, and solution/product/service is now live or operational. Includes after-action reporting to document and evaluate project results. Solution, product or service expected to be iterated or reassessed for improvement over time.
+          </dd>
+      </dl>
+     
+  
+      <div className="coa-About__sidebar__title">Strategic Goals</div>
+      <div className="coa-About__sidebar__plain-text">On April 5, 2017 the Council affirmed both a <a href="https://austinstrategicplan.bloomfire.com/posts/1373079-city-of-austin-strategic-direction">strategic vision</a> and six corresponding strategic outcomes to guide the City of Austin over the next three to five years. 
+      
+   
+     
+      <dl className="coa-about__phase">
+      
+      <p><GovSvg />
+      <dt className="coa-About__sidebar__goal__title"> Government That Works
+      </dt>
+      
+          <dd className="coa-About__sidebar__plain-text">Identifying, qualifying, and scoping a project. Includes leads or opportunities that have just been identified, but haven't yet decided to pursue.
+          </dd>
+      </p>
+
+      <p><MobilitySvg />
+      <dt className="coa-About__sidebar__goal__title">Mobility</dt>
+          <dd className="coa-About__sidebar__plain-text">Includes research, discovery, needs assessment, and requirements gathering
+          </dd>
+      </p>
+
+      <p><HealthSvg />  
+      <dt className="coa-About__sidebar__goal__title">Health</dt>
+          <dd className="coa-About__sidebar__plain-text">Execution and active project work, including design, development, testing, solution implementation.
+          </dd>
+      </p>
+
+      <p><SafetySvg />
+          <dt className="coa-About__sidebar__goal__title">Safety</dt>
+          <dd className="coa-About__sidebar__plain-text">Identifying, qualifying, and scoping a project. Includes leads or opportunities that have just been identified, but haven't yet decided to pursue.
+          </dd>
+      </p>
+      <p><EconomicSvg />
+          <dt className="coa-About__sidebar__goal__title">Economic Opportunity</dt>
+          <dd className="coa-About__sidebar__plain-text">Identifying, qualifying, and scoping a project. Includes leads or opportunities that have just been identified, but haven't yet decided to pursue.
+          </dd>
+      </p>
+      <p><LearningSvg />
+        <dt className="coa-About__sidebar__goal__title">Culture and Lifelong Learning</dt>
+          <dd className="coa-About__sidebar__plain-text">Finished implementation, and solution/product/service is now live or operational. Includes after-action reporting to document and evaluate project results. Solution, product or service expected to be iterated or reassessed for improvement over time.
+          </dd>
+      </p>
+      </dl>
+      </div>
+      </div>
+      </div>
+  </section>
+
+
+  
+
+
+
     
-    When the City of Austin joined the Open Government Partnership’s Subnational Pilot Program in December 2016, we made a commitment to build and test a project tracking tool as part of our participation. In short, we committed to up the City of Austin’s game in transparency, civic participation, accountability, and technology innovation. <a href="https://www.opengovpartnership.org/about/about-ogp" target="_blank" class="link--external">Learn more about the Open Government Partnership</a></div>
-  </div>
-  
-  
-  <div className="coa-about_title">
-  Other City of Austin Project Tracking Tools
-</div>
-<div className="coa-about_body">
-If you are looking for details about projects and programs funded by the 2016 Mobility Bond Program, you can view them on the <a href="http://capitalprojects.austintexas.gov/projects" target="_blank" class="link--external">City of Austin Project Explorer.</a> If you are interested in diving into the City's budget and how funds are allocated, get a guided view through <a href="http://budget.austintexas.gov/#!/year/default"> Open Budget ATX.</a>
-</div>
 
-<div className="coa-about_title">
-Follow our progress and contribute 
-</div>
-<div className="coa-about_body">
-Since this is open government, we are designing and building a project tracking tool using open source code that users can follow, copy and contribute to. We keep our code on <a href="https://github.com/cityofaustin/grackl">Github</a> and welcome collaboration!
 
-</div>
-
-<div className="coa-about_title">
-Get In Touch
-</div>
-<div className="coa-about_body">
-Do you have feedback for us? Or are you a City staff member wanting to publish your project on ATX Project Tracker?
-
-We would love to hear from you:<br/>
-Contact us at the: <a href="mailto:innovation@austintexas.gov">Innovation Office</a>
-
-</div>
-
-  </div>
-  
-  
-
-    
+ 
 )
 
 export const query = graphql`
